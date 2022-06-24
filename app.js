5a5764ca36d4ad3f78961aa88fbb4e5e5b34ea53
Bhuvan Bodhanapati <bhuvanb27@gmail.com>
2022-06-24 20:26:48 +0530

    Hash Password using md5 and Store into DB

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
-const encrypt = require("mongoose-encryption");
+const md5 = require("js-md5");
 
 mongoose.connect("mongodb://localhost:27017/usersDB");
 
@@ -21,11 +21,6 @@ app.use(express.static("public"));
 const userSchema = new mongoose.Schema({
     username : String,
     password : String
-});
-
-userSchema.plugin(encrypt,{
-    secret : process.env.SECRET,
-    encryptedFields : ['password']
 });
 
 const User = new mongoose.model('User', userSchema);
@@ -46,7 +41,7 @@ app.route("/register")
     .post(function(req,res){
         const newUser = new User({
                 username : req.body.username,
-                password : req.body.password
+                password : md5(req.body.password)
         });
         newUser.save(function(err) {
             if(err){
@@ -65,7 +60,7 @@ app.route("/login")
     })
     .post(function(req,res){
         const username = req.body.username;
-        const password = req.body.password;
+        const password = md5(req.body.password);
         User.findOne(
             { username : username},
             function(err,foundUser){
